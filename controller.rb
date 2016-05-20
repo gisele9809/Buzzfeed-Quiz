80ac1c3704bb578e984a8b9d252ec595d5200760
Gisele Monarrez <giselemonarrez@Giseles-MacBook-Air.local>
2016-05-20 10:55:28 -0500

    Quiz

--- controller.rb
+++ controller.rb
@@ -15,15 +15,28 @@ set :database, "sqlite3:mydatabase.sqlite3"
 require './models/models.rb'
 
 get '/' do
-  @post = Post.first
+
   erb :index
 end
 
 get '/Quiz/:id' do
-	@quiz= Quiz.find(params(:id))
+	@quiz= Quiz.find(params[:id].to_i)
 	@questions= @quiz.questions
-	erb :Quiz 
+	erb :quiz 
 end
+
+
 post '/form' do
-  "Hello World"
+@answers= params["1"].to_i + params["2"].to_i + params["3"].to_i 
+	if @answers.between?(1, 4)
+		return "Parents Basement"
+	elsif @answers.between?(5, 7)
+		return "World Traveler" 
+	elsif @answers.between?(8, 10)
+		return "Entrepeneur" 
+	elsif @answers.between?(11, 12)
+		return "CEO"
+	end
+	erb :form
 end
+
--- models/models.rb
+++ models/models.rb
@@ -25,4 +25,3 @@ class Answer < ActiveRecord::Base
 belongs_to :question
 has_one :quiz, through: :question
 end
-ls
